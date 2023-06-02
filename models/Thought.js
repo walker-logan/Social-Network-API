ffca8089922ee0c252f6b48c2fe3d62f01b72365
Walker Logan <walker.logan44@gmail.com>
2023-06-01 21:05:44 -0500

    added index.js to routes api and Thought.js in models

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const ReactionSchema = require('./Reaction');
+
+const ThoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+      minLength: 1,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+    timestamps: true, 
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', ThoughtSchema);
+
+module.exports = Thought;
