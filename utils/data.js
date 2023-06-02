59f836c635b4e9fa6c303e5172cf6a8ab9d863f5
Walker Logan <walker.logan44@gmail.com>
2023-06-01 23:26:27 -0500

    added data

---
+++ utils/data.js
@@ -0,0 +1,34 @@
+const users = [
+  {
+    username: "neilarmstrong",
+    email: "neilarmstrong@nasa.gov"
+  },
+  {
+    username: "buzzaldrin",
+    email: "buzzaldrin@nasa.gov"
+  },
+  {
+    username: "sallyride",
+    email: "sallyride@nasa.gov"
+  }
+]
+
+const thoughts = [
+  {
+    thoughtText: "That's one small step for man, one giant leap for mankind.",
+    createdAt: "07/20/1969",
+    username: "neilarmstrong"
+  },
+  {
+    thoughtText: "Beautiful view. Magnificent desolation.",
+    createdAt: "07/20/1969",
+    username: "buzzaldrin"
+  },
+  {
+    thoughtText: "I can see the earth glowing with life.",
+    createdAt: "06/18/1983",
+    username: "sallyride"
+  }
+]
+
+module.exports = { users, thoughts }
---
+++ utils/seed.js
@@ -0,0 +1,29 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { users, thoughts }= require('./data');
+
+connection.on('error', (err) => {
+    console.error(err);
+    process.exit(1);
+});
+
+connection.once('open', async () => {
+  try {
+    console.log('connected');
+
+    await User.deleteMany({});
+
+    await Thought.deleteMany({});
+
+    await User.insertMany(users);
+
+    await Thought.insertMany(thoughts);
+
+    console.info('Seeding complete.');
+  } catch (error) {
+    console.error('Error during seeding: ', error);
+  } finally {
+    connection.close();
+    process.exit(0);
+  }
+});
