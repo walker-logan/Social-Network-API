526fe4f3f4beb72c7813bb3ebcc5c29bd2505712
Walker Logan <walker.logan44@gmail.com>
2023-06-01 21:44:30 -0500

    added userController

---
+++ models/Reaction.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const ReactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+    timestamps: true,
+  }
+);
+
+module.exports = ReactionSchema;
